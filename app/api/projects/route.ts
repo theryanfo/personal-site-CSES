12ca4af6a0684b1351f136de331f68f41d8d2093
theryanfo <ryan.s.fordham@gmail.com>
2025-12-28 01:11:04 -0800

    added post functionality for experience and projects

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -4,6 +4,21 @@ import Project from "../../../models/Project";
 
 export async function GET() {
     await connectToDatabase();
-    const projects = await Project.find();
+    const projects = await Project.find().sort({ startDate: -1 });
     return NextResponse.json(projects);
+}
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        await connectToDatabase();
+
+        const project = await Project.create(body);
+        return NextResponse.json(project, { status: 201 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to create project" },
+            { status: 400 }
+        );
+    }
 }
